30635695096b523a4e834ab2a21cd513ac9f7a64
truongtuhuy070718 <truogntuhuy2@gmail.com>
2019-09-11 15:55:34 +0700

    fix convention

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use App\Models\Task;
 class TaskController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $tasks = Task::all();
         return view('index')->with('tasks', $tasks);
     }
